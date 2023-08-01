e26248627843cea30f22ec5382e4a500312dee5d
Martin Chavez <martinchavez1203@gmail.com>
2023-08-01 11:52:03 -0600

    challenge 2

---
+++ code_challenges/challenge_2.rb
@@ -0,0 +1,9 @@
+good_driving_record = true
+age = 24
+if good_driving_record == true && age > 25 
+    puts " You get a discount!"
+    elsif good_driving_record == true || age > 25 
+     puts " they should pay full price!"
+    else 
+        puts "you need someone else to sign!"
+    end 
